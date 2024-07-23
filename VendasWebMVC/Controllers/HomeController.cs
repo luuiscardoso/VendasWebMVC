cad133e2523064f8ef6c4b5843606c6188bfbd30
Luis Cardoso <luiscard.queiroz@gmail.com>
2024-07-23 17:21:17 -0300

    Primeira view

--- VendasWebMVC/Controllers/HomeController.cs
+++ VendasWebMVC/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using VendasWebMVC.Models;
 
+
 namespace VendasWebMVC.Controllers
 {
     public class HomeController : Controller
---
+++ VendasWebMVC/Models/mdlDepartment.cs
@@ -0,0 +1,8 @@
+﻿namespace VendasWebMVC.Models
+{
+    public class mdlDepartment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
